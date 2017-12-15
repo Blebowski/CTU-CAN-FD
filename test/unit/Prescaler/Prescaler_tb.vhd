316873d2ea19d4d46920ea97a094611c5825dba4
Ondrej <illeondr@fel.cvut.cz>
2017-12-15 10:28:51 +0100

    License update

--- test/unit/Prescaler/Prescaler_tb.vhd
+++ test/unit/Prescaler/Prescaler_tb.vhd
@@ -1,24 +1,36 @@
 --------------------------------------------------------------------------------
---
+-- 
 -- CAN with Flexible Data-Rate IP Core 
---
--- Copyright (C) 2015 Ondrej Ille <ondrej.ille@gmail.com>
---
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU General Public License
--- as published by the Free Software Foundation; either version 2
--- of the License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- The CAN protocol is developed by Robert Bosch GmbH and     
--- protected by patents. Anybody who wants to implement this    
--- IP core on silicon has to obtain a CAN protocol license
--- from Bosch.
---
+-- 
+-- Copyright (C) 2017 Ondrej Ille <ondrej.ille@gmail.com>
+-- 
+-- Project advisor: Jiri Novak <jnovak@fel.cvut.cz>
+-- Department of Measurement         (http://meas.fel.cvut.cz/)
+-- Faculty of Electrical Engineering (http://www.fel.cvut.cz)
+-- Czech Technical University        (http://www.cvut.cz/)
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy 
+-- of this VHDL component and associated documentation files (the "Component"), 
+-- to deal in the Component without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+-- and/or sell copies of the Component, and to permit persons to whom the 
+-- Component is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in 
+-- all copies or substantial portions of the Component.
+-- 
+-- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHTHOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS 
+-- IN THE COMPONENT.
+-- 
+-- The CAN protocol is developed by Robert Bosch GmbH and protected by patents. 
+-- Anybody who wants to implement this IP core on silicon has to obtain a CAN 
+-- protocol license from Bosch.
+-- 
 --------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------------------------------------
