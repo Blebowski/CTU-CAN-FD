a448f99309bd197597191b061d5898f7cf957646
Ondrej <illeondr@fel.cvut.cz>
2018-02-05 16:20:28 +0100

    Added comments

--- scripts/pyXact_generator/ip_xact/vhdl_addr_generator.py
+++ scripts/pyXact_generator/ip_xact/vhdl_addr_generator.py
@@ -4,7 +4,8 @@
 ##   
 ##   Copyright (C) 2018 Ondrej Ille <ondrej.ille@gmail.com>
 ##
-##   Address map generator to VHDL package with constants.  
+##   Address map generator to VHDL package with constants from IP-XACT memory
+##	 map defined by pyXact framework.
 ## 
 ##	Revision history:
 ##		25.01.2018	First implementation
@@ -27,11 +28,21 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 	
 	
 	def commit_to_file(self):
+		""" 
+		Commit the generator output into the output file.
+		"""
 		for line in self.vhdlGen.out :
 			self.of.write(line)
 	
 	
 	def write_reg_enums(self, field):
+		""" 
+		Write register enums values as VHDL constants (std_logic) into the
+		generator output.
+		Arguments:
+			field		Register field object (parsed from pyXact) whose 
+						enums to write.
+		"""
 		if (field.enumeratedValues == []):
 			return False
 		
@@ -43,8 +54,16 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 				decl = LanDeclaration(e.name, e.value, "std_logic",
 							field.bitWidth, "constant", 50)
 				self.vhdlGen.write_decl(decl)
-				
+	
+		
 	def write_res_vals(self, field):
+		""" 
+		Write restart values of register field as VHDL constants (std_logic)
+		into the generator output.
+		Arguments:
+			field		Register field object (parsed from pyXact) whose 
+						restart values to write.
+		"""
 		if (field.resets == None):
 			return False
 		
@@ -57,6 +76,17 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 
 
 	def write_reg_field(self, field, reg):
+		""" 
+		Write register field indices as VHDL constants. Use the "busWidth"
+		property to concatenate the register indices into word aligned sizes.
+		E.g. register with 0x1 offset will start at index 8, 0x2 at index 16 etc...
+		Write into the generator output.
+		Arguments:
+			field		Register field object (parsed from pyXact) whose 
+						field indices to write.
+			reg			Register object (parsed from pyXact) to which belongs
+						the field in previous argument.
+		"""
 		bitIndexL = field.bitOffset
 		bitIndexH = field.bitOffset + field.bitWidth-1
 	
@@ -75,7 +105,14 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 			
 	
 	def __write_reg(self, reg, writeFields, writeEnums, writeReset):
-		
+		""" 
+		Write the register as set of VHDL constants into the generator output.
+		Arguments:
+			reg			Register field object (parsed from pyXact) to write
+			writeFields	IF bit field indices should be written.
+			writeEnums	If Enums for each bit field should be written.
+			writeReset	If reset values for each field should be written.
+		"""
 		# Write the register title
 		capt = '{} register'.format(reg.name.upper())
 		self.vhdlGen.write_comment(reg.description, 2, caption=capt)
@@ -101,11 +138,22 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 
 	
 	def write_regs(self, regs):
+		""" 
+		Write the multiple registers as sets of VHDL constants.
+		Arguments:
+			regs	List of register objects as parsed by pyxact framework.
+		"""
 		for reg in sorted(regs, key=lambda a: a.addressOffset):
 			self.__write_reg(reg, True, True, True)
 
 
 	def write_addrbl_head(self, addressBlock):
+		""" 
+		Write the address block from IP-XACT memory map as VHDL constant
+		to generator output.
+		Arguments:
+			addressBlock	Address block to write (parsed from pyXact framework)
+		"""
 		# Write capital comment with name of the address Block
 		self.vhdlGen.write_comm_line()
 		self.vhdlGen.write_comment("Address block: {}".format(addressBlock.name), 2)
@@ -122,6 +170,12 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 		
 
 	def write_addrbl_regs(self, addressBlock):
+		""" 
+		Write the address block registers from IP-XACT address block to the
+		generator output.
+		Arguments:
+			addressBlock	Address block to write as parsed by pyXact.
+		"""
 		for reg in sorted(addressBlock.register, key=lambda a: a.addressOffset):
 			decl = LanDeclaration(reg.name+"_ADR",
 						reg.addressOffset+addressBlock.baseAddress, "std_logic",
@@ -130,24 +184,42 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 
 
 	def write_mem_map_addr(self):
+		""" 
+		Write address map specified at instance creation time and stored in
+		"addrMap".
+		"""
 		#Each Address block reflects to VHDL memory region
 		for block in self.addrMap.addressBlock:
 			self.write_addrbl_head(block)
 			self.write_addrbl_regs(block)
 			self.vhdlGen.wr_nl()
 
+
 	def write_mem_map_fields(self):
+		""" 
+		Write field map specified at instance creation time and stored in
+		"addrMap".
+		"""
 		for block in self.fieldMap.addressBlock:
 			self.write_regs(block.register)
 
 
 	def write_mem_map_both(self):
+		""" 
+		Write address and field map specified at instance creation time and
+		stored in "addrMap".
+		"""
 		self.write_mem_map_fields()
 		self.write_mem_map_addr()
 		
 		
 	def create_addrMap_package(self, name):
-		
+		""" 
+		Create a VHDL package to the address generator output. Insert basic
+		IEEE include: std_logic_1164.all
+		Arguments:
+			name	VHDL pacakge name
+		"""
 		self.vhdlGen.wr_nl()
 		self.vhdlGen.write_comm_line(gap=0)
 		if (self.addrMap != None):
@@ -177,6 +249,10 @@ class VhdlAddrGenerator(IpXactAddrGenerator):
 				
 
 	def write_reg(self, reg, writeFields, writeRstVal, writeEnums): 
+		""" 
+		Public version of internal function. TODO: remove the internal one
+		and use the public one.
+		"""
 		self.__write_reg(self, reg, writeFields, writeEnums, writeRstVal)
 
 	
