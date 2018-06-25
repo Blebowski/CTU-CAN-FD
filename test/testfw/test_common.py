bf5d58129b0fd439b351b10bb7e1f29b37d11b28
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2018-06-25 18:00:34 +0200

    Bug-fix

--- test/testfw/test_common.py
+++ test/testfw/test_common.py
@@ -82,7 +82,7 @@ def add_flags(ui, lib, build):
         ut.scan_tests_from_file(str(build / "../unit/vunittb_wrapper.vhd"))
 
     reference_tests = lib.get_test_benches('*reference*', allow_empty=True)
-    for rt in reference_tests
+    for rt in reference_tests:
         rt.scan_tests_from_file(str(build / "../reference/vunit_reference_wrapper.vhd"))
 
     #lib.add_compile_option("ghdl.flags", ["-Wc,-g"])
