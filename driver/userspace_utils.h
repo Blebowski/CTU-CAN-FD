030c3f5a5e5b5636428a38bd9570248225a20a70
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2018-12-08 19:47:11 +0100

    Updated enum name ctu_can_fd_regs to ctu_can_fd_can_registers. Register map was renamed in IP-XACT, thus the re-generation. Maybe a little long, but it looks better in IP-XACT. Otherwise mem map name was just "Regs" which was not what one was seeing in documentation...

--- driver/userspace_utils.h
+++ driver/userspace_utils.h
@@ -49,5 +49,5 @@ extern "C" {
 #include <err.h>
 
 struct ctucanfd_priv* ctucanfd_init(uint32_t addr);
-unsigned ctu_can_fd_read8(struct ctucanfd_priv *priv, enum ctu_can_fd_regs reg);
-unsigned ctu_can_fd_read16(struct ctucanfd_priv *priv, enum ctu_can_fd_regs reg);
+unsigned ctu_can_fd_read8(struct ctucanfd_priv *priv, enum ctu_can_fd_can_registers reg);
+unsigned ctu_can_fd_read16(struct ctucanfd_priv *priv, enum ctu_can_fd_can_registers reg);
