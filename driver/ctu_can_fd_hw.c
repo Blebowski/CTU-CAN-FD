dd50481f2c30e3af42085f16d0a91e10535f9e86
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-01-20 18:03:14 +0100

    driver: Add timestamp read-out function.
    Implemented protection against overflow between read from
    TIMESTAMP_LOW and TIMESTAMP_HIGH word.

--- driver/ctu_can_fd_hw.c
+++ driver/ctu_can_fd_hw.c
@@ -774,4 +774,21 @@ bool ctu_can_fd_insert_frame(struct ctucanfd_priv *priv,
 	return true;
 }
 
+static u64 ctu_can_fd_read_timestamp(struct ctucanfd_priv *priv)
+{
+	union ctu_can_fd_timestamp_low ts_low;
+	union ctu_can_fd_timestamp_high ts_high;
+	union ctu_can_fd_timestamp_high ts_high_2;
+
+    ts_high.u32 = priv->read_reg(priv, CTU_CAN_FD_TIMESTAMP_HIGH);	
+    ts_low.u32 = priv->read_reg(priv, CTU_CAN_FD_TIMESTAMP_LOW);
+	ts_high_2.u32 = priv->read_reg(priv, CTU_CAN_FD_TIMESTAMP_HIGH);
+
+	if (ts_high.u32 != ts_high_2.u32){
+		ts_low.u32 = priv->read_reg(priv, CTU_CAN_FD_TIMESTAMP_LOW);
+	}
+
+	return (( (u64) ts_high_2.u32) << 32) | ( (u64) ts_low.u32);
+}
+
 // TODO: AL_CAPTURE and ERROR_CAPTURE
