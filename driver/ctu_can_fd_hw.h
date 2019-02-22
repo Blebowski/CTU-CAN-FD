da41214db4d320fb3e9f1eccb5f795be4c034a69
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2019-02-22 14:29:42 +0100

    driver: remove paragraph about writing to FSF it is reported as error by checkpatch.pl.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- driver/ctu_can_fd_hw.h
+++ driver/ctu_can_fd_hw.h
@@ -9,8 +9,8 @@
  *     Martin Jerabek <martin.jerabek01@gmail.com>
  *
  * Project advisors:
- *	Jiri Novak <jnovak@fel.cvut.cz>
- *	Pavel Pisa <pisa@cmp.felk.cvut.cz>
+ *     Jiri Novak <jnovak@fel.cvut.cz>
+ *     Pavel Pisa <pisa@cmp.felk.cvut.cz>
  *
  * Department of Measurement         (http://meas.fel.cvut.cz/)
  * Faculty of Electrical Engineering (http://www.fel.cvut.cz)
@@ -25,12 +25,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- *
  ******************************************************************************/
 
 #ifndef __CTU_CAN_FD_HW__
