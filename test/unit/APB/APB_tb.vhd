6fc38cdcdf87eb5f126e3e67a3b3fd230fc546d9
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-06-10 08:38:17 +0200

    src, test: Raise version to 2.2

--- test/unit/APB/APB_tb.vhd
+++ test/unit/APB/APB_tb.vhd
@@ -201,11 +201,11 @@ begin
 
         -- read just after HW reset
         apb_read(DEVICE_ID_ADR);
-        check(s_apb_prdata = x"0201CAFD", "CAN ID reg mismatch (just after HW reset)");
+        check(s_apb_prdata = x"0202CAFD", "CAN ID reg mismatch (just after HW reset)");
 
         apb_write(BTR_ADR, x"FFFFFFFF", b"1111");
         apb_read(DEVICE_ID_ADR);
-        check(s_apb_prdata = x"0201CAFD", "CAN ID reg mismatch");
+        check(s_apb_prdata = x"0202CAFD", "CAN ID reg mismatch");
         apb_read(BTR_ADR);
         check(s_apb_prdata = x"FFFFFFFF", "readback mismatch");
 
