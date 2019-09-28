c4e7a540240118aa10e89fd8881e0d1f58e758cd
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-09-28 21:28:54 +0200

    test: Update byte enable feature test.

--- test/feature/byte_enable_feature_tb.vhd
+++ test/feature/byte_enable_feature_tb.vhd
@@ -41,12 +41,27 @@
 
 --------------------------------------------------------------------------------
 -- Purpose:
---  Feature test for Byte enable functionality.
+--  Byte enable feature test.
 --
+-- Verifies:
+--  1. 8/16/32 bit read access.
+--  2. 8/16/32 bit write access.
+--
+-- Test sequence:
+--  1. Disable Node 1 (so that BTR, BTR_FD are writable). Read 32-bit register
+--     via 32-bit access. Check correct value.
+--  2. Read 32-bit register via 2 16 bit accesses. Check correct values.
+--  3. Read 32-bit register via 4 8 bit accesses. Check correct values.
+--  4. Erase 32-bit R/W register. Write pattern to a single byte. Read 32-bit
+--     check that pattern was written to single byte only and rest are zeroes.
+--     Repeat with each byte!
+--  5. Erase 32-bit R/W register. Write pattern to lower half word. Read 32-bit,
+--     check that pattern was written to half word only and rest are zeroes.
+--     Repeat with upper half word!
 --------------------------------------------------------------------------------
 -- Revision History:
---
---    12.7.2018     Created file 
+--    12.7.2018     Created file
+--    28.9.2019     Accustomized to common feature test format. 
 --------------------------------------------------------------------------------
 
 context work.ctu_can_synth_context;
@@ -86,16 +101,19 @@ package body byte_enable_feature is
         o.outcome := true;
 
         ------------------------------------------------------------------------
-        -- First read full YOLO register (32 BIT)
+        -- 1. Disable Node 1 (so that BTR, BTR_FD are writable). Read 32-bit 
+        --    register via 32-bit access. Check correct value.
         ------------------------------------------------------------------------
+        info("Step 1: 32-bit read");
+        CAN_turn_controller(false, ID, mem_bus(1));
         address := YOLO_REG_ADR;
         CAN_read(data, address, ID, mem_bus(1), BIT_32);
-
         check(data = x"DEADBEEF", "32 bit read error");
 
         ------------------------------------------------------------------------
-        -- Now read per half word (16 BIT)
+        -- 2. Read 32-bit register via 2 16 bit accesses. Check correct values.
         ------------------------------------------------------------------------
+        info("Step 2: 16-bit read");
         for i in 0 to 1 loop        
             data := (OTHERS => '0');
             address := std_logic_vector(to_unsigned(
@@ -113,8 +131,9 @@ package body byte_enable_feature is
         end loop;
 
         ------------------------------------------------------------------------
-        -- Now read per byte (8 BIT)
+        -- 3. Read 32-bit register via 4 8 bit accesses. Check correct values.
         ------------------------------------------------------------------------
+        info("Step 3: 8-bit read");
         for i in 0 to 3 loop        
             data := (OTHERS => '0');
             address := std_logic_vector(to_unsigned(
@@ -148,53 +167,88 @@ package body byte_enable_feature is
             end case;
         end loop;
 
-
         ------------------------------------------------------------------------
-        -- Use R/W register to verify also write functionality!
-        -- First erase the register, then write 0xA to each byte and check
-        -- that it was written only to this byte!
-        -- Use FILTER_A_MASK register and 0xA only since highest byte has only
-        -- 5 bits valid!
+        -- 4. Erase 32-bit R/W register. Write pattern to a single byte. Read 
+        --    32-bit check that pattern was written to single byte only and 
+        --    rest are zeroes. Repeat with each byte!
         ------------------------------------------------------------------------
+        info("Step 4: 8-bit write");
         for i in 0 to 3 loop
-            address := FILTER_A_MASK_ADR;
+            address := BTR_FD_ADR;
             data := (OTHERS => '0');
             CAN_write(data, address, ID, mem_bus(1));
 
             address := std_logic_vector(to_unsigned(
-		            (to_integer(unsigned(FILTER_A_MASK_ADR)) + i), 12));
-            data(8 * i + 7 downto 8 * i) := x"0A";
+		            (to_integer(unsigned(BTR_FD_ADR)) + i), 12));
+            data(8 * i + 7 downto 8 * i) := x"AA";
             CAN_write(data, address, ID, mem_bus(1), BIT_8);
             data := (OTHERS => '0');
 
-            CAN_read(data, address, ID, mem_bus(1), BIT_8);
+            CAN_read(data, BTR_FD_ADR, ID, mem_bus(1), BIT_32);
             
             -- Checking if one written byte was written OK!
-            check(data(8 * i + 7 downto 8 * i) = x"0A",
-                  "Write error - 8 bit (valid byte), Index :");
+            check(data(8 * i + 7 downto 8 * i) = x"AA",
+                  "Write access - 8 bit (valid byte), Index :" & integer'image(i));
 
             -- Checking if other bytes are 0
             case i is
             when 0 =>
                 check(data(31 downto 8) = x"000000",
-                     "Write error - 8 bit (Empty byte 0)");
+                     "Write access - 8 bit (Empty byte 0)");
             when 1 =>
                 check(data(31 downto 16) = x"0000" and
                       data(7 downto 0) = x"00",
-                      "Write error - 8 bit (Empty byte 1)");
+                      "Write access - 8 bit (Empty byte 1)");
             when 2 =>
                 check(data(31 downto 24) = x"00" and
                       data(15 downto 0) = x"0000",
-                      "Write error - 8 bit (Empty byte 2)");
+                      "Write access - 8 bit (Empty byte 2)");
 
             when 3 =>
                 check(data(23 downto 0) = x"000000",
-                      "Write error - 8 bit (Empty byte 3)");
+                      "Write access - 8 bit (Empty byte 3)");
                       
             when others =>
                 error("Invalid byte index"); -- LCOV_EXCL_LINE
             end case;
         end loop;
+        
+        ------------------------------------------------------------------------
+        --  5. Erase 32-bit R/W register. Write pattern to lower half word. Read
+        --     32-bit, check that pattern was written to half word only and rest
+        --     are zeroes. Repeat with upper half word!
+        ------------------------------------------------------------------------
+        info("Step 4: 16-bit write");
+        for i in 0 to 1 loop
+            address := BTR_FD_ADR;
+            data := (OTHERS => '0');
+            CAN_write(data, address, ID, mem_bus(1));
+
+            address := std_logic_vector(to_unsigned(
+                        (to_integer(unsigned(BTR_FD_ADR)) + (i * 2)), 12));
+            data(16 * i + 15 downto 16 * i) := x"AAAA";
+            CAN_write(data, address, ID, mem_bus(1), BIT_16);
+            data := (OTHERS => '0');
+
+            CAN_read(data, BTR_FD_ADR, ID, mem_bus(1), BIT_32);
+            
+            -- Checking if one written byte was written OK!
+            check(data(16 * i + 15 downto 16 * i) = x"AAAA",
+                  "Write access - 16 bit (valid half word), Index :" & integer'image(i));
+
+            -- Checking if other half word is 0
+            case i is
+            when 0 =>
+                check(data(31 downto 16) = x"0000",
+                     "Write access - 16 bit (Empty half word 0)");
+            when 1 =>
+                check(data(15 downto 0) = x"0000",
+                      "Write access - 16 bit (Empty half word 1)");
+
+            when others =>
+                error("Invalid half word index"); -- LCOV_EXCL_LINE
+            end case;
+        end loop;       
 
     end procedure;
 
