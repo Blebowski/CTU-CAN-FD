6ce5f096f5dd67062e4cf8fcf1a4a69c465a5269
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-11-15 06:00:36 +0100

    test: Add BTR_FD feature test.

--- test/feature/btr_fd_feature_tb.vhd
+++ test/feature/btr_fd_feature_tb.vhd
@@ -41,19 +41,21 @@
 
 --------------------------------------------------------------------------------
 -- Purpose:
---  BTR (Bit timing register) feature test.
+--  BTR FD (Bit timing register - flexible data rate) feature test.
 --
 -- Verifies:
---  1. BTR register properly configures PROP, PH1, PH2 registers.
+--  1. BTR FD register properly configures PROP, PH1, PH2 registers.
 --  2. Transmission/reception at random bit-rate.
 --
 -- Test sequence:
---   1. Disable both Nodes. Generate random bit-rate and configure it sa Nominal
---      bit-rate! Enable both nodes and wait till both nodes are on.
---   2. Wait until sample point in Node 1 and measure number of clock cycles
---      till next sample point. Check that it corresponds to pre-computed value!
---   3. Send frame by Node 1 and wait until it is sent. Read frame from Node 2
---      and check they are matching. Do vice-versa from Node 2 to Node 1!
+--   1. Disable both Nodes. Generate random bit-rate and configure it sa Data
+--      bit-rate! Nominal bit-rate remains the default one which was set by
+--      testbench.
+--   2. Enable both Nodes and send CAN FD frame where bit-rate is shifted by
+--      Node 1. Wait until data field in Node 1 and measure duration till next
+--      sample point! Transmitter during Data phase shall no re-synchronize
+--      therefore, bit time will always have nominal length!
+--   3. Wait until frame is sent and check it is correctly received by Node 2.
 --------------------------------------------------------------------------------
 -- Revision History:
 --   11.11.2019   Created file
@@ -64,8 +66,8 @@ context work.ctu_can_test_context;
 
 use lib.pkg_feature_exec_dispath.all;
 
-package btr_feature is
-    procedure btr_feature_exec(
+package btr_fd_feature is
+    procedure btr_fd_feature_exec(
         variable    o               : out    feature_outputs_t;
         signal      so              : out    feature_signal_outputs_t;
         signal      rand_ctr        : inout  natural range 0 to RAND_POOL_SIZE;
@@ -76,8 +78,8 @@ package btr_feature is
 end package;
 
 
-package body btr_feature is
-    procedure btr_feature_exec(
+package body btr_fd_feature is
+    procedure btr_fd_feature_exec(
         variable    o               : out    feature_outputs_t;
         signal      so              : out    feature_signal_outputs_t;
         signal      rand_ctr        : inout  natural range 0 to RAND_POOL_SIZE;
@@ -111,82 +113,85 @@ package body btr_feature is
 
         -----------------------------------------------------------------------
         -- 1. Disable both Nodes. Generate random bit-rate and configure it sa 
-        --    Nominal bit-rate! Enable both nodes and wait till both nodes are 
-        --    on.
+        --    Data bit-rate! Nominal bit-rate remains the default one which was
+        --    set by testbench.
         -----------------------------------------------------------------------
         info("Step 1");
         CAN_turn_controller(false, ID_1, mem_bus(1));
         CAN_turn_controller(false, ID_2, mem_bus(2));
 
-        -- Generate random Nominal bit rate!
-        rand_int_v(rand_ctr, 127, bus_timing.prop_nbt);
-        rand_int_v(rand_ctr, 63, bus_timing.ph1_nbt);
-        rand_int_v(rand_ctr, 63, bus_timing.ph2_nbt);
-        rand_int_v(rand_ctr, 255, bus_timing.tq_nbt);
-        rand_int_v(rand_ctr, 33, bus_timing.sjw_nbt);
+        -- Read timing so that NBT values are kept!
+        CAN_read_timing_v(bus_timing, ID_1, mem_bus(1));
 
-        -----------------------------------------------------------------------
-        -- Leave Data bit rate unconfigured! This will result in XXXs in register
-        -- map, and XXXs in prescaler (we will see about warnings), but in this
-        -- test we will NOT use data bit-rate, therefore all logic operating
-        -- on XXXs should be ignored! So it will be a nice experiment that
-        -- even crazy things in DBT does not ruin NBT operation...
-        -- This test will never have chance to pass at any "X pesimistic"
-        -- simulation. But who has money for X pessimism simulator? :(
-        -----------------------------------------------------------------------
+        -- Generate random Nominal bit rate!
+        rand_int_v(rand_ctr, 63, bus_timing.prop_dbt);
+        rand_int_v(rand_ctr, 31, bus_timing.ph1_dbt);
+        rand_int_v(rand_ctr, 31, bus_timing.ph2_dbt);
+        
+        -- Constrain time quanta to something realistinc for data phase so
+        -- that we don't have too long run times!
+        rand_int_v(rand_ctr, 16, bus_timing.tq_dbt);
+        rand_int_v(rand_ctr, 33, bus_timing.sjw_dbt);
 
         -- SJW should be at least one because clocks differ by some value so
         -- there should be chance to compensate
-        if (bus_timing.sjw_nbt = 0) then
-            bus_timing.sjw_nbt := 1;
+        if (bus_timing.sjw_dbt = 0) then
+            bus_timing.sjw_dbt := 1;
         end if;
 
         -- Constrain minimal BRP (0 is not allowed)!
-        if (bus_timing.tq_nbt = 0) then
-            bus_timing.tq_nbt := 1;
+        if (bus_timing.tq_dbt = 0) then
+            bus_timing.tq_dbt := 1;
         end if;
 
         -- Pre-calculate expected number of clock cycles
-        clock_per_bit := (1 + bus_timing.prop_nbt + bus_timing.ph1_nbt +
-                          bus_timing.ph2_nbt) * bus_timing.tq_nbt;
+        clock_per_bit := (1 + bus_timing.prop_dbt + bus_timing.ph1_dbt +
+                          bus_timing.ph2_dbt) * bus_timing.tq_dbt;
 
-        -- It has no sense to test frequencies above 5 MHz for Nominal Bit-rate
+        -- It has no sense to test bit times where there is less than 5 clocks per bit!
         -- lets constrain it to something reasonable.
-        while (clock_per_bit < 20) loop
+        while (clock_per_bit < 6) loop
 
-            if (bus_timing.prop_nbt < 127) then
-                bus_timing.prop_nbt := bus_timing.prop_nbt + 1;
+            if (bus_timing.prop_dbt < 127) then
+                bus_timing.prop_dbt := bus_timing.prop_dbt + 1;
             end if;
 
-            if (bus_timing.ph1_nbt < 63) then
-                bus_timing.ph1_nbt := bus_timing.ph1_nbt + 1;
+            if (bus_timing.ph1_dbt < 63) then
+                bus_timing.ph1_dbt := bus_timing.ph1_dbt + 1;
             end if;
 
-            if (bus_timing.ph2_nbt < 63) then
-                bus_timing.ph2_nbt := bus_timing.ph2_nbt + 1;
+            if (bus_timing.ph2_dbt < 63) then
+                bus_timing.ph2_dbt := bus_timing.ph2_dbt + 1;
             end if;
 
-            clock_per_bit := (1 + bus_timing.prop_nbt + bus_timing.ph1_nbt +
-                              bus_timing.ph2_nbt) * bus_timing.tq_nbt;
+            clock_per_bit := (1 + bus_timing.prop_dbt + bus_timing.ph1_dbt +
+                              bus_timing.ph2_dbt) * bus_timing.tq_dbt;
         end loop;
 
         -- Constrain minimal duration of PH2 to be 2 clock cycles!
-        if (bus_timing.ph2_nbt * bus_timing.prop_nbt < 2) then
-            bus_timing.ph2_nbt := 2;
+        if (bus_timing.ph2_dbt * bus_timing.prop_dbt < 2) then
+            bus_timing.ph2_dbt := 2;
         end if;
 
         -- Constrain minimal duration of TSEG1 to be 2 clock cycles!
-        if ((bus_timing.prop_nbt + bus_timing.ph1_nbt + 1) * bus_timing.tq_nbt < 2) then
-            bus_timing.prop_nbt := 1;
+        if ((bus_timing.prop_dbt + bus_timing.ph1_dbt + 1) * bus_timing.tq_dbt < 2) then
+            bus_timing.prop_dbt := 1;
         end if;
 
         -- Pre-calculate expected number of clock cycles after all corrections!
-        clock_per_bit := (1 + bus_timing.prop_nbt + bus_timing.ph1_nbt +
-                          bus_timing.ph2_nbt) * bus_timing.tq_nbt;
+        clock_per_bit := (1 + bus_timing.prop_dbt + bus_timing.ph1_dbt +
+                          bus_timing.ph2_dbt) * bus_timing.tq_dbt;
 
         CAN_configure_timing(bus_timing, ID_1, mem_bus(1));
         CAN_configure_timing(bus_timing, ID_2, mem_bus(2));
 
+        -----------------------------------------------------------------------
+        -- 2. Enable both Nodes and send CAN FD frame where bit-rate is shifted
+        --    by Node 1. Wait until data field in Node 1 and measure duration
+        --    till next sample point! Transmitter during Data phase shall no
+        --    re-synchronize therefore, bit time will always have nominal length!
+        -----------------------------------------------------------------------
+        info("Step 2");
         CAN_turn_controller(true, ID_1, mem_bus(1));
         CAN_turn_controller(true, ID_2, mem_bus(2));
 
@@ -199,15 +204,32 @@ package body btr_feature is
         info("PH2: " & integer'image(bus_timing.ph2_nbt));
         info("SJW: " & integer'image(bus_timing.sjw_nbt));
 
-        -----------------------------------------------------------------------
-        -- 2. Wait until sample point in Node 1 and measure number of clock
-        --    cycles till next sample point. Check that it corresponds to
-        --    pre-computed value!
-        -----------------------------------------------------------------------
-        info("Step 2");
+        info("CAN bus Data bit-rate:");
+        info("PROP: " & integer'image(bus_timing.prop_dbt));
+        info("PH1: " & integer'image(bus_timing.ph1_dbt));
+        info("PH2: " & integer'image(bus_timing.ph2_dbt));
+        info("SJW: " & integer'image(bus_timing.sjw_dbt));
+
+        CAN_generate_frame(rand_ctr, CAN_frame_1);
+        CAN_frame_1.brs := BR_SHIFT;
+        CAN_frame_1.frame_format := FD_CAN;
+    
+        -- Force DLC length to 1 byte only not to have long test run time!
+        CAN_frame_1.dlc := "0001";
+        decode_dlc(CAN_frame_1.dlc, CAN_frame_1.data_length);
+        CAN_frame_1.data(0) := x"AA";
+        CAN_frame_1.data(1) := x"BB";
+        CAN_frame_1.data(2) := x"CC";
+        CAN_frame_1.data(3) := x"DD";
+    
+        CAN_send_frame(CAN_frame_1, 1, ID_1, mem_bus(1), frame_sent);
+        CAN_wait_pc_state(pc_deb_data, ID_1, mem_bus(1));
+
         CAN_wait_sample_point(iout(1).stat_bus, false);
+        
         wait for 11 ns;
 
+        -- Measure duration till next Sample point!
         while (iout(1).stat_bus(STAT_RX_TRIGGER) = '0') loop
             clock_meas := clock_meas + 1;
             wait until rising_edge(mem_bus(1).clk_sys);
@@ -215,17 +237,14 @@ package body btr_feature is
 
         check(clock_per_bit = clock_meas,
             " Expected clock per bit: " & integer'image(clock_per_bit) &
-            " Measured clock per bit: " & integer'image(clock_meas));
+            " Measured clock per bit: " & integer'image(clock_meas));        
 
         -----------------------------------------------------------------------
-        -- 3. Send frame by Node 1 and wait until it is sent. Read frame from
-        --    Node 2 and check they are matching. Do vice-versa from Node 2 to
-        --    Node 1!
+        -- 3. Wait until frame is sent and check it is correctly received by
+        --    Node 2.
         -----------------------------------------------------------------------
         info("Step 3");
-        CAN_generate_frame(rand_ctr, CAN_frame_1);
-        CAN_send_frame(CAN_frame_1, 1, ID_1, mem_bus(1), frame_sent);
-        CAN_wait_frame_sent(ID_2, mem_bus(2));
+        CAN_wait_bus_idle(ID_2, mem_bus(2));
         CAN_read_frame(CAN_frame_2, ID_2, mem_bus(2));
 
         CAN_compare_frames(CAN_frame_1, CAN_frame_2, false, frames_equal);
