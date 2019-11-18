d3d6659e33fc642452e31a630882db437e1a6fae
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-11-18 13:53:22 +0100

    reg_map: Reduce TRV_DELAY to its true width!

--- driver/ctu_can_fd_regs.h
+++ driver/ctu_can_fd_regs.h
@@ -857,7 +857,8 @@ union ctu_can_fd_trv_delay_ssp_cfg {
 	struct ctu_can_fd_trv_delay_ssp_cfg_s {
 #ifdef __LITTLE_ENDIAN_BITFIELD
   /* TRV_DELAY */
-		uint32_t trv_delay_value        : 16;
+		uint32_t trv_delay_value         : 7;
+		uint32_t reserved_15_7           : 9;
   /* SSP_CFG */
 		uint32_t ssp_offset              : 7;
 		uint32_t reserved_23             : 1;
@@ -868,7 +869,8 @@ union ctu_can_fd_trv_delay_ssp_cfg {
 		uint32_t ssp_src                 : 2;
 		uint32_t reserved_23             : 1;
 		uint32_t ssp_offset              : 7;
-		uint32_t trv_delay_value        : 16;
+		uint32_t reserved_15_7           : 9;
+		uint32_t trv_delay_value         : 7;
 #endif
 	} s;
 };
