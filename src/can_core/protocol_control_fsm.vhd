9ff91352a58979958abd8aa8d9005aea892db062
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2019-12-09 23:00:56 +0100

    src: IDE in arbitration not in control for receiver!
    When we don't transmitt anything, keep IDE in Arbitration field,
    not in Control field! This is better because we won't receive
    short is_control pulse for the duration of IDE bit!! We don't mind
    that in case of Error, this will be always interpreted as error in
    Arbitration field even if BASE frame is being received!

--- src/can_core/protocol_control_fsm.vhd
+++ src/can_core/protocol_control_fsm.vhd
@@ -781,7 +781,7 @@ begin
                          else
                      '0';
 
-    ide_is_arbitration <= '1' when (tran_ident_type = EXTENDED)
+    ide_is_arbitration <= '1' when (tran_ident_type = EXTENDED or is_receiver = '1')
                               else
                           '0';
 
