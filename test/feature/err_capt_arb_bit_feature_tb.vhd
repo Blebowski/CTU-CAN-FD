327c152b2a34b6da1b1070a674f87209601db883
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2020-01-03 09:26:36 +0100

    test: Add ERR_CAPT arbitration - bit error feature test.

--- test/feature/err_capt_arb_bit_feature_tb.vhd
+++ test/feature/err_capt_arb_bit_feature_tb.vhd
@@ -41,19 +41,21 @@
 
 --------------------------------------------------------------------------------
 -- Purpose:
---  ERR_CAPT[ERR_POS] = ERC_POS_SOF feature test. 
+--  ERR_CAPT[ERR_POS] = ERC_POS_ARB, bit error feature test. 
 --
 -- Verifies:
---  1. Detection of form error in SOF bit.
+--  1. Detection of bit error in Arbitration field. Value of ERR_CAPT[ERR_POS]
+--     when bit error should have been detected in arbitration field.
 --
 -- Test sequence:
---  1. Generate CAN frame and send it by Node 1. Wait until transmission starts
---     and force bus Recessive. Wait until sample point and check that Error
---     frame is transmitted. Check that ERR_CAPT says that Form Error during
---     SOF was detected!
+--  1. Check that ERR_CAPT contains no error (post reset).
+--  2. Generate CAN frame and send it by Node 1. Wait until transmission starts
+--     and wait until arbitration field. Wait for random amount of time until
+--     Dominant bit is sent! Force bus low and wait until sample point. Check
+--     that Error frame is being transmitted. Check value of ERR_CAPT.
 --------------------------------------------------------------------------------
 -- Revision History:
---    02.02.2020   Created file
+--    03.02.2020   Created file
 --------------------------------------------------------------------------------
 
 context work.ctu_can_synth_context;
@@ -61,8 +63,8 @@ context work.ctu_can_test_context;
 
 use lib.pkg_feature_exec_dispath.all;
 
-package err_capt_sof_feature is
-    procedure err_capt_sof_feature_exec(
+package err_capt_arb_bit_feature is
+    procedure err_capt_arb_bit_feature_exec(
         signal      so              : out    feature_signal_outputs_t;
         signal      rand_ctr        : inout  natural range 0 to RAND_POOL_SIZE;
         signal      iout            : in     instance_outputs_arr_t;
@@ -72,8 +74,8 @@ package err_capt_sof_feature is
 end package;
 
 
-package body err_capt_sof_feature is
-    procedure err_capt_sof_feature_exec(
+package body err_capt_arb_bit_feature is
+    procedure err_capt_arb_bit_feature_exec(
         signal      so              : out    feature_signal_outputs_t;
         signal      rand_ctr        : inout  natural range 0 to RAND_POOL_SIZE;
         signal      iout            : in     instance_outputs_arr_t;
@@ -122,17 +124,49 @@ package body err_capt_sof_feature is
     begin
 
         -----------------------------------------------------------------------
-        -- 1. Generate CAN frame and send it by Node 1. Wait until transmission
-        --    starts and force bus Recessive. Wait until sample point and check
-        --    that Error frame is transmitted. Check that ERR_CAPT says that
-        --    Form Error during SOF was detected!
+        -- 1. Check that ERR_CAPT contains no error (post reset).
         -----------------------------------------------------------------------
         info("Step 1");
-
+        
+        CAN_read_error_code_capture(err_capt, ID_1, mem_bus(1));
+        check(err_capt.err_pos = err_pos_other, "Reset of ERR_CAPT!");
+        
+        -----------------------------------------------------------------------        
+        --  2. Generate CAN frame and send it by Node 1. Wait until transmission
+        --     starts and wait until arbitration field. Wait for random amount
+        --     of time until Dominant bit is sent! Force bus low and wait until
+        --     sample point. Check that Error frame is being transmitted. Check
+        --     value of ERR_CAPT.
+        -----------------------------------------------------------------------
+        info("Step 2");
+        
         CAN_generate_frame(rand_ctr, frame_1);
+        frame_1.ident_type := EXTENDED;
         CAN_send_frame(frame_1, 1, ID_1, mem_bus(1), frame_sent);
         CAN_wait_tx_rx_start(true, false, ID_1, mem_bus(1));
+        
+        CAN_wait_pc_state(pc_deb_arbitration, ID_1, mem_bus(1));
+        
+        -- Wait time is adjusted so that we are sure that we will still be in
+        -- arbitration field (of base or extended). After 26 bits, if there are
+        -- all dominant till end of frame, we are sure at least one stuff bit
+        -- will be there!
+        rand_int_v(rand_ctr, 25, wait_time);
+        info ("Waiting for:" & integer'image(wait_time) & " bits!");
 
+        for i in 1 to wait_time loop
+            CAN_wait_sync_seg(iout(1).stat_bus);
+            info("Wait sync");
+            wait for 20 ns;
+        end loop;
+        info("Waiting finished!");
+
+        while (iout(1).can_tx = RECESSIVE) loop
+            CAN_wait_sync_seg(iout(1).stat_bus);
+            wait for 20 ns;
+        end loop;
+        
+        -- Force bus for one bit time
         force_bus_level(RECESSIVE, so.bl_force, so.bl_inject);
         CAN_wait_sample_point(iout(1).stat_bus, false);
         wait for 20 ns; -- To be sure that opposite bit is sampled!
@@ -142,8 +176,8 @@ package body err_capt_sof_feature is
         check (stat_1.error_transmission, "Error frame is being transmitted!");
         
         CAN_read_error_code_capture(err_capt, ID_1, mem_bus(1));
-        check(err_capt.err_type = can_err_form, "Form error detected!");
-        check(err_capt.err_pos = err_pos_sof, "Error detected in SOF!");
+        check(err_capt.err_type = can_err_bit, "Bit error detected!");
+        check(err_capt.err_pos = err_pos_arbitration, "Error detected in Arbitration!");
         
         CAN_wait_bus_idle(ID_1, mem_bus(1));
 
