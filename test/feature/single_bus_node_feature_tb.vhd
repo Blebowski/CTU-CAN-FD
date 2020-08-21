ee0a2b67666eb248f0de2f2ae3fcc76b59376028
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2020-08-21 19:58:16 +0200

    src, test: Split compilation into two libs: RTL and TB

--- test/feature/single_bus_node_feature_tb.vhd
+++ test/feature/single_bus_node_feature_tb.vhd
@@ -65,10 +65,11 @@
 --    18.7.2020   Created file
 --------------------------------------------------------------------------------
 
-context work.ctu_can_synth_context;
-context work.ctu_can_test_context;
+Library ctu_can_fd_tb;
+context ctu_can_fd_tb.ctu_can_synth_context;
+context ctu_can_fd_tb.ctu_can_test_context;
 
-use lib.pkg_feature_exec_dispath.all;
+use ctu_can_fd_tb.pkg_feature_exec_dispath.all;
 
 package single_bus_node_feature is
     procedure single_bus_node_feature_exec(
