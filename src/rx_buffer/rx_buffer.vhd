a32059c3cd409a84c112bf6630cd894601736e2d
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2020-12-12 13:50:07 +0100

    src: Relax coverpoint to match how SW/driver operates on the RX Buffer.

--- src/rx_buffer/rx_buffer.vhd
+++ src/rx_buffer/rx_buffer.vhd
@@ -955,8 +955,10 @@ begin
     --      cover {(read_increment = '1')[*4]};
     --
     -- psl rx_buf_burst_read_max_cov :
-    --      cover {(read_increment = '1')[*20]};
-    --
+    --      cover {(read_increment = '1')[*19]};
+    -- Note: SW reads the frame like so: Read one word, then burst of 19 to read rest.
+    --       Therefore highest burst achievable is 19!
+    
     -- psl rx_buf_frame_abort_cov :
     --      cover {rec_abort_f = '1'};
     --
