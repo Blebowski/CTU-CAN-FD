0096a0f09b217c8a8656e5a89f0592daaed6c80f
Ille, Ondrej, Ing <illeondr@fel.cvut.cz>
2021-02-04 20:41:25 +0100

    src, test: Change license

--- src/lib/can_components.vhd
+++ src/lib/can_components.vhd
@@ -1,7 +1,34 @@
 --------------------------------------------------------------------------------
 -- 
--- CTU CAN FD IP Core
--- Copyright (C) 2015-2018
+-- CTU CAN FD IP Core 
+-- Copyright (C) 2021-present Ondrej Ille
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this VHDL component and associated documentation files (the "Component"),
+-- to use, copy, modify, merge, publish, distribute the Component for
+-- educational, research, evaluation, self-interest purposes. Using the
+-- Component for commercial purposes is forbidden unless previously agreed with
+-- Copyright holder.
+-- 
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Component.
+-- 
+-- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHTHOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
+-- IN THE COMPONENT.
+-- 
+-- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
+-- Anybody who wants to implement this IP core on silicon has to obtain a CAN
+-- protocol license from Bosch.
+-- 
+-- -------------------------------------------------------------------------------
+-- 
+-- CTU CAN FD IP Core 
+-- Copyright (C) 2015-2020 MIT License
 -- 
 -- Authors:
 --     Ondrej Ille <ondrej.ille@gmail.com>
@@ -36,7 +63,7 @@
 -- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
 -- Anybody who wants to implement this IP core on silicon has to obtain a CAN
 -- protocol license from Bosch.
---
+-- 
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
