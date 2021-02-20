ea5ad40e56f1f0bf4b85b588f7b094e62c485afa
Ille, Ondrej, Ing <ondrej.ille@gmail.com>
2021-02-20 18:29:48 +0100

    Update APB_tb.vhd

--- test/unit/APB/APB_tb.vhd
+++ test/unit/APB/APB_tb.vhd
@@ -246,7 +246,7 @@ begin
 
         apb_write(BTR_ADR, x"FFFFFFFF", b"1111");
         apb_read(DEVICE_ID_ADR);
-        check(s_apb_prdata = x"0202CAFD", "CAN ID reg mismatch");
+        check(s_apb_prdata = x"0203CAFD", "CAN ID reg mismatch");
         apb_read(BTR_ADR);
         check(s_apb_prdata = x"FFFFFFFF", "readback mismatch");
 
