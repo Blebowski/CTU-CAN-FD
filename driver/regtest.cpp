5d48e93cab0587f3152a5471113b3b2d9c20905b
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2021-03-04 21:48:40 +0100

    driver: update core revision to 2.3 in register test - even the second test.
    Closes #409.
    
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- driver/regtest.cpp
+++ driver/regtest.cpp
@@ -144,7 +144,7 @@ int main(int argc, char *argv[])
 
     apb_write(CTU_CAN_FD_BTR, 0xFFFFFFFF, 0b1111);
     apb_read(CTU_CAN_FD_DEVICE_ID);
-    CHECK(0x0202CAFD, "CAN ID reg mismatch");
+    CHECK(0x0203CAFD, "CAN ID reg mismatch");
 
     apb_read(CTU_CAN_FD_BTR);
     CHECK(0xFFFFFFFF, "readback mismatch");
