e12019b0e1811e0d8471f42447cd2e8ca1cc4eda
Ondrej Ille <ondrej.ille@gmail.com>
2021-05-21 15:26:12 +0200

    ci: Add gate level sims to CI .

--- synthesis/Vivado/ci_benchmark/benchmark_configs.tcl
+++ synthesis/Vivado/ci_benchmark/benchmark_configs.tcl
@@ -39,21 +39,6 @@ set DESIGN_CONFIGS [list \
         ]
     ] \
     [ dict create \
-     name "big_design_config" \
-     generics \
-        [ dict create \
-         rx_buffer_size         512 \
-         txt_buffer_count       8 \
-         sup_filt_A             "1'b1" \
-         sup_filt_B             "1'b1" \
-         sup_filt_C             "1'b1" \
-         sup_range              "1'b1" \
-         target_technology      1 \
-         sup_traffic_ctrs       "1'b1" \
-         sup_test_registers     "1'b0" \
-        ]
-    ] \
-    [ dict create \
      name "maximal_design_config" \
      generics \
         [ dict create \
