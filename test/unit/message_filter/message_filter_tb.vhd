fc04aad462657e83bcc86998e3fc29213dd176aa
Ondrej Ille <ondrej.ille@gmail.com>
2021-06-04 12:01:07 +0200

    test: Adjust tests for new type of binding indications.

--- test/unit/message_filter/message_filter_tb.vhd
+++ test/unit/message_filter/message_filter_tb.vhd
@@ -100,9 +100,8 @@ use STD.textio.all;
 library ctu_can_fd_rtl;
 use ctu_can_fd_rtl.id_transfer_pkg.all;
 use ctu_can_fd_rtl.can_constants_pkg.all;
-use ctu_can_fd_rtl.can_components_pkg.all;
+
 use ctu_can_fd_rtl.can_types_pkg.all;
-use ctu_can_fd_rtl.common_blocks_pkg.all;
 use ctu_can_fd_rtl.drv_stat_pkg.all;
 use ctu_can_fd_rtl.unary_ops_pkg.all;
 use ctu_can_fd_rtl.can_config_pkg.all;
@@ -307,7 +306,7 @@ begin
     ----------------------------------------------------------------------------
     -- Instance of frame filters
     ----------------------------------------------------------------------------
-    frame_filters_comp : frame_filters
+    frame_filters_comp : entity ctu_can_fd_rtl.frame_filters
     PORT map(
         clk_sys            =>  clk_sys,
         res_n              =>  res_n,
