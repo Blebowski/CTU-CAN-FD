6e665a2e250c30453943776bdde2faaa66c9d567
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2024-06-19 14:22:01 +0200

    doc, spec: Rename LBPF_REMOTE to LBPF_FOREIGN to avoid confusion with remote frames.

--- driver/ctucanfd_frame.h
+++ driver/ctucanfd_frame.h
@@ -105,8 +105,8 @@ enum ctu_can_fd_frame_format_w_fdf {
 };
 
 enum ctu_can_fd_frame_format_w_lbpf {
-	LBPF_REMOTE       = 0x0,
-	LBPF_LOCAL        = 0x1,
+	LBPF_FOREIGN       = 0x0,
+	LBPF_LOCAL         = 0x1,
 };
 
 enum ctu_can_fd_frame_format_w_brs {
