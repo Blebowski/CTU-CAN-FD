efc92c6c083437e0d24c68f9b76c1a5b4d5403a1
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2024-06-19 14:48:57 +0200

    test: Extend Loop-back test to test also LBPF value.

--- test/main_tb/feature_tests/mode_loopback_ftest.vhd
+++ test/main_tb/feature_tests/mode_loopback_ftest.vhd
@@ -1,18 +1,18 @@
 --------------------------------------------------------------------------------
--- 
--- CTU CAN FD IP Core 
+--
+-- CTU CAN FD IP Core
 -- Copyright (C) 2021-present Ondrej Ille
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this VHDL component and associated documentation files (the "Component"),
 -- to use, copy, modify, merge, publish, distribute the Component for
 -- educational, research, evaluation, self-interest purposes. Using the
 -- Component for commercial purposes is forbidden unless previously agreed with
 -- Copyright holder.
--- 
+--
 -- The above copyright notice and this permission notice shall be included in
 -- all copies or substantial portions of the Component.
--- 
+--
 -- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,38 +20,38 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 -- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
 -- IN THE COMPONENT.
--- 
+--
 -- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
 -- Anybody who wants to implement this IP core on silicon has to obtain a CAN
 -- protocol license from Bosch.
--- 
+--
 -- -------------------------------------------------------------------------------
--- 
--- CTU CAN FD IP Core 
+--
+-- CTU CAN FD IP Core
 -- Copyright (C) 2015-2020 MIT License
--- 
+--
 -- Authors:
 --     Ondrej Ille <ondrej.ille@gmail.com>
 --     Martin Jerabek <martin.jerabek01@gmail.com>
--- 
--- Project advisors: 
+--
+-- Project advisors:
 -- 	Jiri Novak <jnovak@fel.cvut.cz>
 -- 	Pavel Pisa <pisa@cmp.felk.cvut.cz>
--- 
+--
 -- Department of Measurement         (http://meas.fel.cvut.cz/)
 -- Faculty of Electrical Engineering (http://www.fel.cvut.cz)
 -- Czech Technical University        (http://www.cvut.cz/)
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this VHDL component and associated documentation files (the "Component"),
 -- to deal in the Component without restriction, including without limitation
 -- the rights to use, copy, modify, merge, publish, distribute, sublicense,
 -- and/or sell copies of the Component, and to permit persons to whom the
 -- Component is furnished to do so, subject to the following conditions:
--- 
+--
 -- The above copyright notice and this permission notice shall be included in
 -- all copies or substantial portions of the Component.
--- 
+--
 -- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -59,11 +59,11 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 -- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
 -- IN THE COMPONENT.
--- 
+--
 -- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
 -- Anybody who wants to implement this IP core on silicon has to obtain a CAN
 -- protocol license from Bosch.
--- 
+--
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
@@ -85,17 +85,24 @@
 --  @2. Generate random CAN frame and send it by DUT.
 --  @3. Wait until frame is received. Check that DUT has 1 frame in RX Buffer.
 --  @4. Read CAN frame from DUT. Check it is the same as transmitted frame.
---      Check that there are 0 frames in RX Buffer of DUT. Read also frame
---      from Test node not to leave it hanging there!
---  @5. Set Test node to Acknowledge forbidden mode. Set DUT to one shot mode.
---  @6. Generate random CAN frame and send it by DUT.
---  @7. Wait until transmission is over. Check that TXT Buffer used for transmi-
+--      Check the frame contains LBPF flag set.
+--      Check that there are 0 frames in RX Buffer of DUT.
+--      Read the frame from Test node not to leave it hanging there!
+--  @5. Generate random frame and send it by Test Node. Wait until the frame
+--      is sent, and recived in DUT. Read the frame from DUT. Check it matches
+--      the transmitted frame. Check it has LBPF flag set to 0.
+--  @6. Set Test node to Acknowledge forbidden mode. Set DUT to one shot mode.
+--  @7. Generate random CAN frame and send it by DUT.
+--  @8. Wait until transmission is over. Check that TXT Buffer used for transmi-
 --      ssion is in TX failed. Check that RX Buffer in DUT has no frame.
---  @8. Disable Loopback mode in DUT. Disable Acknowledge forbidden mode in
+--  @9. Disable Loopback mode in DUT. Disable Acknowledge forbidden mode in
 --      Test node.
---  @9. Send CAN frame by DUT. Wait until frame is over.
--- @10. Check that RX Buffer of DUT has no CAN frame received. Check that
+-- @10. Send CAN frame by DUT. Wait until frame is over.
+-- @11. Check that RX Buffer of DUT has no CAN frame received. Check that
 --      RX Buffer of Test node has frame received.
+-- @12. Generate random frame and send it by Test Node. Wait until the frame
+--      is sent, and recived in DUT. Read the frame from DUT. Check it matches
+--      the transmitted frame. Check it has LBPF flag set to 0.
 --
 -- @TestInfoEnd
 --------------------------------------------------------------------------------
@@ -136,7 +143,7 @@ package body mode_loopback_ftest is
         -- @1. Configure Loopback mode in DUT.
         ------------------------------------------------------------------------
         info_m("Step 1: Configuring Loopback mode in DUT");
-        
+
         mode_1.internal_loopback := true;
         set_core_mode(mode_1, DUT_NODE, chn);
 
@@ -144,113 +151,157 @@ package body mode_loopback_ftest is
         -- @2. Generate random CAN frame and send it by DUT.
         ------------------------------------------------------------------------
         info_m("Step 2: Sending frame by DUT");
-        
+
         CAN_generate_frame(CAN_TX_frame);
         CAN_send_frame(CAN_TX_frame, 1, DUT_NODE, chn, frame_sent);
-        
+
         ------------------------------------------------------------------------
-        -- @3. Wait until frame is received. Check that DUT has 1 frame in 
+        -- @3. Wait until frame is received. Check that DUT has 1 frame in
         --     RX Buffer.
         ------------------------------------------------------------------------
         info_m("Step 3: Waiting until frame is sent");
-        
+
         CAN_wait_frame_sent(DUT_NODE, chn);
         get_rx_buf_state(rx_buf_state, DUT_NODE, chn);
         check_m(rx_buf_state.rx_frame_count = 1, "Own frame in Loopback received");
 
         ------------------------------------------------------------------------
-        -- @4. Read CAN frame from DUT. Check it is the same as transmitted 
-        --    frame. Check that there are 0 frames in RX Buffer of DUT.
+        -- @4. Read CAN frame from DUT. Check it is the same as transmitted frame.
+        --      Check the frame contains LBPF flag set.
+        --      Check that there are 0 frames in RX Buffer of DUT.
+        --      Read the frame from Test node not to leave it hanging there!
         ------------------------------------------------------------------------
         info_m("Step 4: Read own transmitted frame from RX Buffer");
-        
+
         CAN_read_frame(CAN_RX_frame, DUT_NODE, chn);
         CAN_compare_frames(CAN_RX_frame, CAN_TX_frame, false, frames_equal);
         check_m(frames_equal, "Own frame in Loopback is the same as sent!");
-        
+
+        check_m(CAN_RX_frame.lbpf = '1', "RX Frame has LBPF flag set!");
+
         get_rx_buf_state(rx_buf_state, DUT_NODE, chn);
         check_m(rx_buf_state.rx_frame_count = 0, "Own frame read from RX Buffer");
-        
+
         CAN_read_frame(CAN_RX_frame, TEST_NODE, chn);
-        
+
         ------------------------------------------------------------------------
-        -- @5. Set Test node to Acknowledge forbidden mode. Set DUT to one shot
-        --    mode.
+        -- @5. Generate random frame and send it by Test Node. Wait until the
+        --     frame is sent, and recived in DUT. Read the frame from DUT.
+        --     Check it matches the transmitted frame. Check it has LBPF flag
+        --     set to 0.
         ------------------------------------------------------------------------
-        info_m("Step 5: Configure Test node to ACF, DUT to One shot mode");
-        
+        info_m("Step 5: Transmit frame by Test Node when DUT has Loopback enabled");
+
+        CAN_generate_frame(CAN_TX_frame);
+        CAN_send_frame(CAN_TX_frame, 1, TEST_NODE, chn, frame_sent);
+
+        CAN_wait_frame_sent(TEST_NODE, chn);
+        CAN_wait_bus_idle(DUT_NODE, chn);
+
+        CAN_read_frame(CAN_RX_frame, DUT_NODE, chn);
+        CAN_compare_frames(CAN_RX_frame, CAN_TX_frame, false, frames_equal);
+
+        check_m(frames_equal, "Frame sent by Test Node and received by DUT are equal!");
+        check_m(CAN_RX_frame.lbpf = '0', "Frame from Test Node does not have LBPF flag set!");
+
+        ------------------------------------------------------------------------
+        -- @6. Set Test node to Acknowledge forbidden mode. Set DUT to one shot
+        --     mode.
+        ------------------------------------------------------------------------
+        info_m("Step 6: Configure Test node to ACF, DUT to One shot mode");
+
         mode_2.acknowledge_forbidden := true;
         set_core_mode(mode_2, TEST_NODE, chn);
         CAN_enable_retr_limit(true, 0, DUT_NODE, chn);
-        
+
         ------------------------------------------------------------------------
-        -- @6. Generate random CAN frame and send it by DUT.
+        -- @7. Generate random CAN frame and send it by DUT.
         ------------------------------------------------------------------------
-        info_m("Step 6: Send frame by DUT!");
-        
+        info_m("Step 7: Send frame by DUT!");
+
         CAN_generate_frame(CAN_TX_frame);
         CAN_send_frame(CAN_TX_frame, 1, DUT_NODE, chn, frame_sent);
-        
+
         ------------------------------------------------------------------------
-        -- @7. Wait until transmission is over. Check that TXT Buffer used for 
-        --    transmission is in TX failed. Check that RX Buffer in DUT has 
-        --    no frame.
+        -- @8. Wait until transmission is over. Check that TXT Buffer used for
+        --     transmission is in TX failed. Check that RX Buffer in DUT has
+        --     no frame.
         ------------------------------------------------------------------------
-        info_m("Step 7: Check no own frame received on Error frame!");
-        
+        info_m("Step 8: Check no own frame received on Error frame!");
+
         CAN_wait_error_frame(DUT_NODE, chn);
         CAN_wait_bus_idle(DUT_NODE, chn);
-        
+
         get_tx_buf_state(1, txt_buf_state, DUT_NODE, chn);
         check_m(txt_buf_state = buf_failed, "TXT Buffer failed");
-        
+
         get_rx_buf_state(rx_buf_state, DUT_NODE, chn);
         check_m(rx_buf_state.rx_frame_count = 0,
             "No own frame received when error frame was received!");
 
         ------------------------------------------------------------------------
-        -- @8. Disable Loopback mode in DUT. Disable Acknowledge forbidden 
-        --    mode in Test node.
+        -- @9. Disable Loopback mode in DUT. Disable Acknowledge forbidden
+        --     mode in Test node.
         ------------------------------------------------------------------------
-        info_m("Step 8: Disable Loopback in DUT. Disable ACF in Test node!");
-        
+        info_m("Step 9: Disable Loopback in DUT. Disable ACF in Test node!");
+
         mode_1.internal_loopback := false;
         set_core_mode(mode_1, DUT_NODE, chn);
-        
+
         mode_2.acknowledge_forbidden := false;
         set_core_mode(mode_2, TEST_NODE, chn);
-        
+
         ------------------------------------------------------------------------
-        -- @9. Send CAN frame by DUT. Wait until frame is over.
+        -- @10. Send CAN frame by DUT. Wait until frame is over.
         ------------------------------------------------------------------------
-        info_m("Step 9: Send CAN frame by DUT.");
-        
+        info_m("Step 10: Send CAN frame by DUT.");
+
         CAN_generate_frame(CAN_TX_frame);
         CAN_send_frame(CAN_TX_frame, 1, DUT_NODE, chn, frame_sent);
-        
+
         CAN_wait_frame_sent(DUT_NODE, chn);
         CAN_wait_bus_idle(DUT_NODE, chn);
-        
+
         ------------------------------------------------------------------------
-        -- @10. Check that RX Buffer of DUT has no CAN frame received. Check 
-        --     that RX Buffer of Test node has frame received.
+        -- @11. Check that RX Buffer of DUT has no CAN frame received. Check
+        --      that RX Buffer of Test node has frame received.
         ------------------------------------------------------------------------
-        info_m("Step 10: Check own frame not received when Loopback is disabled");
-        
+        info_m("Step 11: Check own frame not received when Loopback is disabled");
+
         get_rx_buf_state(rx_buf_state, DUT_NODE, chn);
         check_m(rx_buf_state.rx_frame_count = 0,
             "Own frame not received when Loopback mode is disabled!");
-        
+
         get_rx_buf_state(rx_buf_state, TEST_NODE, chn);
         check_m(rx_buf_state.rx_frame_count = 1,
             "Frame received in Test node!");
-        
+
         CAN_read_frame(CAN_RX_frame, TEST_NODE, chn);
         CAN_compare_frames(CAN_RX_frame, CAN_TX_frame, false, frames_equal);
         check_m(frames_equal, "TX vs. RX frame matching!");
 
+        ------------------------------------------------------------------------
+        -- @5. Generate random frame and send it by Test Node. Wait until the
+        --     frame is sent, and recived in DUT. Read the frame from DUT.
+        --     Check it matches the transmitted frame. Check it has LBPF flag
+        --     set to 0.
+        ------------------------------------------------------------------------
+        info_m("Step 12: Transmit frame by Test Node when DUT has Loopback disabled");
+
+        CAN_generate_frame(CAN_TX_frame);
+        CAN_send_frame(CAN_TX_frame, 1, TEST_NODE, chn, frame_sent);
+
+        CAN_wait_frame_sent(TEST_NODE, chn);
+        CAN_wait_bus_idle(DUT_NODE, chn);
+
+        CAN_read_frame(CAN_RX_frame, DUT_NODE, chn);
+        CAN_compare_frames(CAN_RX_frame, CAN_TX_frame, false, frames_equal);
+
+        check_m(frames_equal, "Frame sent by Test Node and received by DUT are equal!");
+        check_m(CAN_RX_frame.lbpf = '0', "Frame from Test Node does not have LBPF flag set!");
+
         wait for 1000 ns;
-        
+
   end procedure;
 
 end package body;
