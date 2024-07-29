a880b6f7fead91049cd0f1fde55a27e91a47ef0a
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2024-07-29 12:48:47 +0200

    test: Add pc_fsm_transitions_integ feature test.

--- test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
+++ test/main_tb/agents/feature_test_agent/feature_test_list_pkg.vhd
@@ -175,6 +175,7 @@ use ctu_can_fd_tb.overload_ftest.all;
 
 use ctu_can_fd_tb.pc_fsm_transitions_err_pas_ftest.all;
 use ctu_can_fd_tb.pc_fsm_transitions_err_act_ftest.all;
+use ctu_can_fd_tb.pc_fsm_transitions_integ_ftest.all;
 
 use ctu_can_fd_tb.rec_saturation_ftest.all;
 use ctu_can_fd_tb.retr_limit_ftest.all;
@@ -414,6 +415,8 @@ package body feature_test_list_pkg is
             pc_fsm_transitions_err_pas_ftest_exec(channel);
         elsif (test_name = "pc_fsm_transitions_err_act") then
             pc_fsm_transitions_err_act_ftest_exec(channel);
+        elsif (test_name = "pc_fsm_transitions_integ") then
+            pc_fsm_transitions_integ_ftest_exec(channel);
 
         elsif (test_name = "rec_saturation") then
             rec_saturation_ftest_exec(channel);
