9cd802e9e7aa768a04c90009ef8e20839f6f928f
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2024-08-29 12:39:57 +0200

    test: Debug rx_buf_empty_read_ftest.
    When empty buffer is read, a frame read may be invalid -> assertions
    in CAN_read_frame should not fire!

--- test/main_tb/feature_tests/rx_buf_empty_read_ftest.vhd
+++ test/main_tb/feature_tests/rx_buf_empty_read_ftest.vhd
@@ -1,18 +1,18 @@
 --------------------------------------------------------------------------------
--- 
--- CTU CAN FD IP Core 
+--
+-- CTU CAN FD IP Core
 -- Copyright (C) 2021-present Ondrej Ille
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this VHDL component and associated documentation files (the "Component"),
 -- to use, copy, modify, merge, publish, distribute the Component for
 -- educational, research, evaluation, self-interest purposes. Using the
 -- Component for commercial purposes is forbidden unless previously agreed with
 -- Copyright holder.
--- 
+--
 -- The above copyright notice and this permission notice shall be included in
 -- all copies or substantial portions of the Component.
--- 
+--
 -- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,38 +20,38 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 -- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
 -- IN THE COMPONENT.
--- 
+--
 -- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
 -- Anybody who wants to implement this IP core on silicon has to obtain a CAN
 -- protocol license from Bosch.
--- 
+--
 -- -------------------------------------------------------------------------------
--- 
--- CTU CAN FD IP Core 
+--
+-- CTU CAN FD IP Core
 -- Copyright (C) 2015-2020 MIT License
--- 
+--
 -- Authors:
 --     Ondrej Ille <ondrej.ille@gmail.com>
 --     Martin Jerabek <martin.jerabek01@gmail.com>
--- 
--- Project advisors: 
+--
+-- Project advisors:
 -- 	Jiri Novak <jnovak@fel.cvut.cz>
 -- 	Pavel Pisa <pisa@cmp.felk.cvut.cz>
--- 
+--
 -- Department of Measurement         (http://meas.fel.cvut.cz/)
 -- Faculty of Electrical Engineering (http://www.fel.cvut.cz)
 -- Czech Technical University        (http://www.cvut.cz/)
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this VHDL component and associated documentation files (the "Component"),
 -- to deal in the Component without restriction, including without limitation
 -- the rights to use, copy, modify, merge, publish, distribute, sublicense,
 -- and/or sell copies of the Component, and to permit persons to whom the
 -- Component is furnished to do so, subject to the following conditions:
--- 
+--
 -- The above copyright notice and this permission notice shall be included in
 -- all copies or substantial portions of the Component.
--- 
+--
 -- THE COMPONENT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -59,11 +59,11 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 -- FROM, OUT OF OR IN CONNECTION WITH THE COMPONENT OR THE USE OR OTHER DEALINGS
 -- IN THE COMPONENT.
--- 
+--
 -- The CAN protocol is developed by Robert Bosch GmbH and protected by patents.
 -- Anybody who wants to implement this IP core on silicon has to obtain a CAN
 -- protocol license from Bosch.
--- 
+--
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
@@ -109,10 +109,11 @@ package body rx_buf_empty_read_ftest is
         -- Generated frames
         variable frame_rx           :     SW_CAN_frame_type;
         variable rx_buf_info        :     SW_RX_Buffer_info;
+        variable rx_data            :     std_logic_vector(31 downto 0);
     begin
 
         -----------------------------------------------------------------------
-        -- @1. Read pointers from RX Buffer, check pointers are 0 (DUT is post 
+        -- @1. Read pointers from RX Buffer, check pointers are 0 (DUT is post
         --     reset).
         -----------------------------------------------------------------------
         info_m("Step 1: Reading RX buffer pointers for first time");
@@ -120,13 +121,18 @@ package body rx_buf_empty_read_ftest is
         get_rx_buf_state(rx_buf_info, DUT_NODE, chn);
         check_m(rx_buf_info.rx_read_pointer = 0, "Read pointer 0!");
         check_m(rx_buf_info.rx_write_pointer = 0, "Write pointer 0!");
-        
+
         -----------------------------------------------------------------------
-        -- @2. Try to read CAN frame from RX Buffer. This should generate at 
+        -- @2. Try to read CAN frame from RX Buffer. This should generate at
         --     least 4 reads from RX DATA register.
         -----------------------------------------------------------------------
         info_m("Step 2: Try to read frame from empty RX Buffer!");
-        CAN_read_frame(frame_rx, DUT_NODE, chn);
+
+        -- Use purposefully "raw" reads instead of CAN_Read_frame
+        CAN_read(rx_data, RX_DATA_ADR, DUT_NODE, chn);
+        CAN_read(rx_data, RX_DATA_ADR, DUT_NODE, chn);
+        CAN_read(rx_data, RX_DATA_ADR, DUT_NODE, chn);
+        CAN_read(rx_data, RX_DATA_ADR, DUT_NODE, chn);
 
         ------------------------------------------------------------------------
         -- @3. Read pointers from RX Buffer, check pointers are still 0.
